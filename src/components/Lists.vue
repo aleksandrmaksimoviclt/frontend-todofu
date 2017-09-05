901c43163756c3a6c0e99ea22dea7f8b029e45f6
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-09-05 14:37:02 +0200

    Saving work

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -1,22 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="list-wrapper">
+    <div class="list">
+
+      <div class="list-header">
+        <div contenteditable="true" class="list-header-name"> To do</div>
+      </div>
+
+      <div class="list-cards">
+        <div class="list-card">
+          <div class="list-card-details">
+            <span class="list-card-title">
+              Some card
+            </span>
+          </div>
+        </div>
+        <div class="list-card">
+          <div class="list-card-details">
+            <span class="list-card-title">
+              Some card
+            </span>
+          </div>
+        </div>
+        <div class="list-card">
+          <div class="list-card-details">
+            <span class="list-card-title">
+              Some card
+            </span>
+          </div>
+        </div>
+        <div class="list-card">
+          <div class="list-card-details">
+            <span class="list-card-title">
+              Some card
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-composer">
+        <div class="list-card">
+          <div class="list-card-details">
+            <textarea dir="auto" style="overflow: hidden; word-wrap: break-word; resize: none; height: 54px;" class="list-card-composer-textarea"></textarea>
+          </div>
+        </div>
+
+        <div class="cc-controls">
+          <div class="cc-controls-section">
+            <input class="primary confirm" type="submit" value="Add">
+            <a class="icon-lg icon-close dark-hover js-cancel" href="#">X</a>
+          </div>
+        </div>
+      </div>
+
+      <a class="open-card-composer js-open-card-composer" href="#">Add a card…</a>
+    </div>
   </div>
 </template>
 
@@ -33,21 +70,90 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
+.list-wrapper{
+  width: 270px;
+  margin: 0 5px;
+  height: 100%;
+  box-sizing: border-box;
   display: inline-block;
-  margin: 0 10px;
+  vertical-align: top;
+  white-space: nowrap;
 }
 
-a {
-  color: #42b983;
+.list {
+  background: #E2E4E6;
+  border-radius: 3px;
+  box-sizing: border-box;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  max-height: 100%;
+  position: relative;
+  white-space: normal;
+}
+
+.list-header {
+  -webkit-box-flex: 0;
+  -webkit-flex: 0 0 auto;
+  -ms-flex: 0 0 auto;
+  flex: 0 0 auto;
+  padding: 8px 10px;
+  position: relative;
+  min-height: 18px;
+}
+
+.list-cards {
+  -webkit-flex: 1 1 auto;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  overflow-y: auto;
+  margin: 0 4px;
+  padding: 0 4px;
+  z-index: 1;
+  min-height: 0;
+}
+
+.list-card {
+  background-color: #fff;
+  border-bottom: 1px solid #ccc;
+  border-radius: 3px;
+  cursor: pointer;
+  margin-bottom: 6px;
+  max-width: 300px;
+  min-height: 20px;
+  position: relative;
+  z-index: 0;
+}
+
+.list-card-details {
+  overflow: hidden;
+  padding: 6px 6px 2px 8px;
+  position: relative;
+  z-index: 10;
+}
+
+.list-card-title {
+  clear: both;
+  font-weight: 400;
+  margin: 0 0 4px;
+  overflow: hidden;
+  word-wrap: break-word;
+  color: #4d4d4d;
+}
+
+.open-card-composer {
+  border-radius: 0 0 3px 3px;
+  color: #8c8c8c;
+  display: block;
+  -webkit-box-flex: 0;
+  -webkit-flex: 0 0 auto;
+  -ms-flex: 0 0 auto;
+  flex: 0 0 auto;
+  padding: 8px 10px;
+  position: relative;
+  text-decoration: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 </style>
