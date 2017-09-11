6a2e1338895221f82798c58997e1c5a443a5302b
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-09-11 15:32:20 +0200

    Add app structure and css

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
+import Board from './components/board/board';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="surface">
         <Header />
+        <div id="content">
+          <Board />
+        </div>
       </div>
     );
   }
