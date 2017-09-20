47c16e124cec25fc10c6340754109a0ea4957f38
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-09-20 12:05:12 +0200

    List delete

--- react/src/components/board/list/list.js
+++ react/src/components/board/list/list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios';
 
 import './list.css';
 
@@ -8,6 +9,14 @@ import CardComposer from './card/card-composer.js';
 
 export default class List extends React.Component {
 
+  handleListDelete = (listID) => {
+    Axios
+    .delete("http://api.todofu.com/v1/lists/" + listID + "/")
+    .then(response => {
+      window.location.reload();
+    });
+  }
+
   render() {
 
     return (
@@ -16,6 +25,9 @@ export default class List extends React.Component {
           <div className="list-header">
             <textarea defaultValue={this.props.name} className="list-header-name mod-list-name">
             </textarea>
+            <div className="list-header-extras">
+              <a onClick={() => this.handleListDelete(this.props.id)} className="fa fa-trash-o icon"></a>
+            </div>
           </div>
           <div className="list-cards">
             {this.props.cards.map(function(card){
